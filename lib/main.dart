722a583ae11ae7bf8fc23696b379f6b331bf014a
suthirakprom <suthirakprom81@gmail.com>
2020-07-16 22:24:31 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/jin.jpg'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
